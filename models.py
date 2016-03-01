9fe7992dd767751e1fbd5d8ea8860441081b0740
Norore <nolwenn.lavielle@gmail.com>
2016-03-01 19:41:12 +0100

    Replace @commit_on_success by the new decorator: @atomic

--- models.py
+++ models.py
@@ -8,6 +8,7 @@ from django.db import IntegrityError
 from django.contrib.auth.models import User
 from django.core.urlresolvers import reverse
 from django.template.defaultfilters import slugify
+from django.db.transaction import atomic
 # from django.db.transaction import commit_on_success <- removed
 
 VERSION = 2
@@ -55,6 +56,7 @@ class MembershipInfo( models.Model ) :
 		}
 
 	# @commit_on_success
+        @atomic
 	def make_user( self ) :
 		if self.user is not None :
 			return None
