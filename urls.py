53a0dd709c7e4ac6dbb05d4cc4f924c80ea5c18f
Norore <nolwenn.lavielle@gmail.com>
2016-03-08 23:06:10 +0100

    Connection to django auth ok

--- urls.py
+++ urls.py
@@ -1,12 +1,10 @@
 
 from django.conf.urls import *
 from django.views.generic import TemplateView
-#from jebif.membership.views import *
 from membership import views
 
 urlpatterns = patterns('',
 	('^subscription/$', views.subscription),
-	#('^subscription/ok/$', TemplateViews.as_view(template="membership/subscription-ok.html")),
         ('^subscription/ok/$', TemplateView.as_view(template_name = "membership/subscription-ok.html")),
 	('^subscription/(?P<info_id>\d+)/renew/$', views.subscription_renew),
 	('^subscription/(?P<info_id>\d+)/update/$', views.subscription_update),
